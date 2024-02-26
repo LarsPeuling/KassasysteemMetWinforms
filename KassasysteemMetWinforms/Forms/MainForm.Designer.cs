a9db383232660040d9ece9701c28a9c86baa476b
LarsPeuling <lpeuling@gmail.com>
2024-02-26 13:50:14 +0100

    Add project files.

---
+++ KassasysteemMetWinforms/Forms/MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace KassasysteemMetWinforms.Forms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnExit = new Button();
+            btnCustomer = new Button();
+            btnProduct = new Button();
+            SuspendLayout();
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(713, 415);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(75, 23);
+            btnExit.TabIndex = 0;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // btnCustomer
+            // 
+            btnCustomer.Location = new Point(713, 309);
+            btnCustomer.Name = "btnCustomer";
+            btnCustomer.Size = new Size(75, 23);
+            btnCustomer.TabIndex = 1;
+            btnCustomer.Text = "Customer";
+            btnCustomer.UseVisualStyleBackColor = true;
+            // 
+            // btnProduct
+            // 
+            btnProduct.Location = new Point(713, 192);
+            btnProduct.Name = "btnProduct";
+            btnProduct.Size = new Size(75, 23);
+            btnProduct.TabIndex = 2;
+            btnProduct.Text = "Product";
+            btnProduct.UseVisualStyleBackColor = true;
+            btnProduct.Click += btnProduct_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnProduct);
+            Controls.Add(btnCustomer);
+            Controls.Add(btnExit);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "MainForm";
+            Text = "Main";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnExit;
+        private Button btnCustomer;
+        private Button btnProduct;
+    }
+}
---
+++ KassasysteemMetWinforms/Forms/ProductForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace KassasysteemMetWinforms.Forms
+{
+    partial class ProductForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnExit = new Button();
+            SuspendLayout();
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(713, 415);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(75, 23);
+            btnExit.TabIndex = 1;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // ProductForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnExit);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "ProductForm";
+            Text = "Product";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnExit;
+    }
+}
